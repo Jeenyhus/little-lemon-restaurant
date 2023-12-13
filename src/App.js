80a29ee88c55a607e76e3298f36a48df760186a0
Jeenyhus <mweembadabwitso@gmail.com>
2023-12-13 14:33:32 +0200

    styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Nav from "./components/Nav";
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>Little Lemon Reloaded</h2>
-    </div>
-  );
+    return (
+        <Router>
+            <div>
+                <Nav />
+                <Routes>
+                    <Route path="/" element={<Header />} />
+                    <Route path="/main" element={<Main />} />
+                </Routes>
+                <Footer />
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,35 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import Header from './components/Header';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Renders the Header heading', () => {
+    render(<BrowserRouter><App /></BrowserRouter>);
+    
+    const headingElement = screen.getByText("Reserve Table");
+    expect(headingElement).toBeInTheDocument();
+
+    const reserveButton = screen.getByLabelText("Reserve Table");
+    userEvent.click(reserveButton);
+
+    const headingElementNew = screen.getByText("Choose Date");
+    expect(headingElementNew).toBeInTheDocument();
+});
+
+test('Initialize/Update Times', () => {
+    render(<BrowserRouter><Header /></BrowserRouter>);
+
+    const reserveButton = screen.getByLabelText("Reserve Table");
+    userEvent.click(reserveButton);
+
+    // Continue to test logic here, e.g., select a date and time
+    // For example, if your BookingForm allows selecting a date and time:
+    const dateInput = screen.getByLabelText("Select Date");
+    userEvent.type(dateInput, '2023-12-25');
+
+    const timeSelect = screen.getByLabelText("Select Time");
+    userEvent.selectOptions(timeSelect, '18:00');
+
+    // Add your assertions based on the expected behavior
 });
